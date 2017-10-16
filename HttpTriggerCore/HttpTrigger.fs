0426b820e1f1c5c1a94f12a868008a24d9d472c3
Mikhail Shilkov <mshilkov@astrata.eu>
2017-10-16 23:50:59 +0200

    Initial implementation

---
+++ HttpTriggerCore/HttpTrigger.fs
@@ -0,0 +1,18 @@
+namespace HttpTriggerCore
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Primitives
+open Microsoft.AspNetCore.Http
+open Microsoft.Azure.WebJobs.Host
+open System.Linq
+
+[<AbstractClass; Sealed>]
+type HttpTrigger private () =
+  static member Run(req: HttpRequest, log: TraceWriter) =
+    log.Info("F# HTTP trigger function processed a request.")
+
+    let found, value = req.Query.TryGetValue "name"
+    if found then    
+      OkObjectResult("Hi, " + value.First()) :> IActionResult
+    else
+      BadRequestObjectResult "Please pass a name on the query string or in the request body" :> IActionResult
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# FSharpHttpCore
-F# Http trigger function for Functions 2.0
+# F# HTTP Trigger Function for .NET Core 2.0
+HTTP trigger function for Functions 2.0. 
+F# version of [this sample](https://raw.githubusercontent.com/lindydonna/CSharpHttpCore).
+Companion to blog post [Develop Azure Functions on any platform](http://blogs.msdn.microsoft.com/appserviceteam/2017/09/25/develop-azure-functions-on-any-platform/).
